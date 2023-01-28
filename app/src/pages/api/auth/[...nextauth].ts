4ea6fcadea31f6bf51bfa15051f3f9c9190e2dd5
Rainaldo Crosbourne <rcrosbourne@Rainaldos-MacBook-Pro.local>
2023-01-28 11:14:26 -0500

    Finally got that fucking sign in to work

--- app/src/pages/api/auth/[...nextauth].ts
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -1,38 +1,63 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
+import NextAuth, {type NextAuthOptions} from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import jwt from "jsonwebtoken";
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db";
+import {env} from "../../../env/server.mjs";
+import {prisma} from "../../../server/db";
+import {session} from "next-auth/core/routes";
+import {JWT} from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
+    pages: {
+     signIn: "/auth/signin",
     },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+    session: {
+        strategy: "jwt",
+    },
+    jwt: {
+        async encode({token}) {
+            return jwt.sign(token as {}, env.NEXTAUTH_SECRET!);
+        },
+        async decode({token}) {
+            return jwt.verify(token!, env.NEXTAUTH_SECRET!) as JWT;
+        },
+    },
+    // Include user.id on session
+    callbacks: {
+        session({session, user, token}) {
+            console.log("session", session, user, token);
+            if (session.user) {
+                session.user.id = token.sub ?? "";
+            }
+            return session;
+        },
+    },
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            type: "credentials",
+            id: "credentials",
+            credentials: {
+                email: {label: "Email", type: "text", placeholder: "jsmith@gmail.com"},
+                password: {label: "Password", type: "password"},
+            },
+            authorize: async (credentials) => {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.email,
+                    }
+                });
+                if (!user) {
+                    return null;
+                }
+                return user;
+            },
+        }),
+    ],
 };
-
 export default NextAuth(authOptions);
